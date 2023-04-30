3ebc19ae4426fedb8b84a777a17125a7074b635b
MKortmann <companymvms@gmail.com>
2023-04-30 17:29:23 +0200

    bug: fix unterminated code string at package.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+const port = 3000;
 
 const add5 = (value: number) => value + 5;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 export default add5;
