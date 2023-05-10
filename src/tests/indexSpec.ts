90e9a78bd55560bb3784c9b33428352b0220bcd1
MKortmann <companymvms@gmail.com>
2023-05-10 16:36:35 +0200

    add: test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,14 +1,12 @@
-// import { Request, Response } from 'express';
-import add5 from '../index';
+// // import { Request, Response } from 'express';
+// import add5 from '../index';
 
-describe('Image EndPoint test', () => {
-  it('should return a 200 status code', () => {
-    // const req: Request = {} as Request;
-    // const res: Response = {} as Response;
+// describe('Image EndPoint test', () => {
+//   it('should return a 200 status code', () => {
 
-    expect(200).toBe(200);
-  });
-  it('should addd 5', () => {
-    expect(add5(5)).toEqual(10);
-  });
-});
+//     expect(200).toBe(200);
+//   });
+//   it('should addd 5', () => {
+//     expect(add5(5)).toEqual(10);
+//   });
+// });
